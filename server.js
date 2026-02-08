0b222fdfb41d1be45a403192396abc86755e5879
Abbey <emmi34.et@gmail.com>
2026-02-08 18:58:17 +0100

    feat: Add production server URL to Swagger configuration

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ const swaggerOptions = {
       {
         url: `http://localhost:${process.env.PORT || 5000}`,
         description: 'Development server'
+      },
+      {
+        url: `https://jobguard-api.onrender.com`,
+        description: 'Production server'
       }
     ],
     components: {
